66d5d72975b779e1a19c09d9098cc5d345cb73b9
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-04 02:02:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//creating a function that takes two parameters and add them
+function addNumber(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumber(5, 10));
---
+++ index.ts
@@ -0,0 +1,8 @@
+//creating a function that takes two parameters and add them
+function addNumber (num1:number, num2:number):number{
+    return num1 + num2
+
+}
+
+//calling two numbers and doing what is given in the return
+console.log(addNumber(5,10));
